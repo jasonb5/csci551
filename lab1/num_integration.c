8ec31f61c3c9fd2bdce7cf7747aa6c6670c65758
Jason Boutte <jboutte@mail.csuchico.edu>
2015-03-11 09:14:46 -0700

    Adds in result printing

--- lab1/num_integration.c
+++ lab1/num_integration.c
@@ -3,12 +3,18 @@
 
 #include <mpi.h>
 
+#define ABS(x)(((x)<0)?-(x):(x))
+#define MAX_ERROR 0.5*pow(10, -12)
+
+double calc_rel_te(double actual, double approx);
 double calc_trap(double a, double b, int n, double h);
+double calc_actual(double a, double b);
 double func(double x);
+double func_int(double x);
 void get_user_input(int rank, double *a, double *b, int *n);
 
 int main(int argc, char **argv) {
-	double a, b, h;
+	double a, b, h, rel_te;
 	double local_a, local_b, local_int, total_int;
 	int n, local_n;
 	int comm_sz, comm_rank;
@@ -34,16 +40,22 @@ int main(int argc, char **argv) {
 	MPI_Reduce(&local_int, &total_int, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
 	if (comm_rank == 0) {
+    rel_te = calc_rel_te(calc_actual(a, b), total_int);
+
 		printf("Running on %d processors.\n", comm_sz);
 		printf("Elapsed time = %e seconds\n", 0.0);
 		printf("With n = %d trapezoids,\n", n);
 		printf("our estimate of the integral from %lf to %lf = %.13e\n", a, b, total_int);
-		printf("absolute relative true error = %e is less than criteria = %e\n", 0.0, 0.0);
+		printf("absolute relative true error = %e %sis less than criteria = %e\n", rel_te, ((rel_te > MAX_ERROR) ? "NOT " : ""),  MAX_ERROR);
 	}	
 
 	MPI_Finalize();
 
 	return 0;
+}
+
+double calc_rel_te(double actual, double approx) {
+  return ABS(actual-approx)*100/actual;
 }
 
 double calc_trap(double a, double b, int n, double h) {
@@ -57,8 +69,16 @@ double calc_trap(double a, double b, int n, double h) {
 	return h*approx;
 }
 
+double calc_actual(double a, double b) {
+  return func_int(b)-func_int(a);
+}
+
 double func(double x) {
 	return 8+5*sin(x/4)-2*cos(x/5)+cos(x/3);
+}
+
+double func_int(double x) {
+  return 8*x-10*sin(x/5)+3*sin(x/3)-20*cos(x/4);
 }
 
 void get_user_input(int rank, double *a, double *b, int *n) {
@@ -68,7 +88,7 @@ void get_user_input(int rank, double *a, double *b, int *n) {
 		scanf("%lf%lf%d", a, b, n);
 	}
 
-	MPI_Bcast(&a, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&b, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(a, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast(b, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast(n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 }
